a795df630776cc02f79617696f8b22a9415e1de2
drollinger <drollingerdallin@gmail.com>
2019-03-26 18:37:30 -0600

    All done with everything. It is running good and no signs of any major bugs. Still have to comment things up though

--- src/ReadCode.java
+++ src/ReadCode.java
@@ -4,11 +4,34 @@ import java.io.File;
 
 
 public class ReadCode {
-    public static LeftistHeap<Term> binarySearcher(String wordStart, ArrayList<Term> listOfWords, int listSize) {
+    static private int badInputCheckerForInt() {
+        Scanner in = new Scanner(System.in);
+        do {
+            try {
+                return in.nextInt();
+            }
+            catch (java.util.InputMismatchException ex) {
+                System.out.print("Please use a number this time: ");
+                in.nextLine();
+            }
+        } while (true);
+    }
+
+
+    private static LeftistHeap<Term> createSpecifiedHeap(String input, ArrayList<Term> listOfWords, int listSize) {
+        LeftistHeap<Term> combinedHeap = new LeftistHeap<>();
+        String[] listOfOrs = input.toLowerCase().replaceAll("\\s", "").split("\\|");
+        for(String prefix : listOfOrs) {
+            combinedHeap.merge(binarySearcher(prefix, listOfWords, listSize));
+        }
+        return combinedHeap;
+    }
+
+    private static LeftistHeap<Term> binarySearcher(String wordStart, ArrayList<Term> listOfWords, int listSize) {
         LeftistHeap<Term> priorityWords = new LeftistHeap<>();
         String currentWord;
-        int length = listSize;
-        int position = length/2;
+        int length = listSize/2;
+        int position = length;
         boolean found = false;
         boolean inList = true;
         while(!found && inList) {
@@ -16,7 +39,6 @@ public class ReadCode {
             currentWord = listOfWords.get(position).word;
 
             if(currentWord.startsWith(wordStart)) {
-                Term temp;
                 int preindecies = position;
 
                 do {
@@ -44,13 +66,14 @@ public class ReadCode {
 
     public static void main(String[] args) {
         try {
-            LeftistHeap<Term> leftHeap = new LeftistHeap<>();
+            LeftistHeap<Term> leftHeap;
             ArrayList<Term> wordArray = new ArrayList<>();
 
             String filename = "SortedWords.txt";
             Scanner reader = new Scanner(new File(filename));
 
             int length = reader.nextInt();
+            boolean userWantsToPlay = true;
 
             while ((reader.hasNext())) {
                 String word = reader.next();
@@ -58,19 +81,42 @@ public class ReadCode {
                 wordArray.add(new Term(word, freq));
             }
 
+            System.out.println("Welcome to Autocomplete!!!");
+
             Scanner in = new Scanner(System.in);
-            System.out.print("Enter string: ");
-            String input = in.nextLine();
+            while(userWantsToPlay) {
+                System.out.print("\nEnter the prefix of the word (use | to \"or\" two words): ");
 
+                String input = in.nextLine();
+                while (!input.matches("[a-zA-Z |]+")) {
+                    System.out.print("Please only use characters and \"|\". Try again: ");
+                    input = in.nextLine();
+                }
 
+                leftHeap = createSpecifiedHeap(input, wordArray, length);
 
-            leftHeap = binarySearcher(input, wordArray, length);
+                System.out.print("Enter the number of outputs: ");
+                int count = badInputCheckerForInt();
 
-            for(int i = 0; i < 10; i++) {
-                System.out.println(leftHeap.deleteMax());
+                System.out.println("\nSubstring: " + input + "\nCount: " + count);
+
+                Term maxValue;
+                for (int i = 0; i < count; i++) {
+                    maxValue = leftHeap.deleteMax();
+                    if(maxValue == null) i = count;
+                    else System.out.println("   " + maxValue.word);
+                }
+
+                do {
+                    System.out.print("\nAgain?(y or n): ");
+                    input = in.nextLine();
+                    if (input.toLowerCase().equals("n") || input.toLowerCase().equals("no")) {
+                        userWantsToPlay = false;
+                    }
+                } while(!(input.toLowerCase().equals("n") || input.toLowerCase().equals("no") || input.toLowerCase().equals("y") || input.toLowerCase().equals("yes")));
             }
-
             reader.close();
+            in.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
